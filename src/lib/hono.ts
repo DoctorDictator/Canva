90db3f4b30ba4ef171c6e2951077d263e3775663
Harsh Shishodia <harshshishodia08@gmail.com>
2025-07-29 23:03:35 +0530

    Update hono.ts

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -1,5 +1,10 @@
 import { hc } from "hono/client";
-
 import { AppType } from "@/app/api/[[...route]]/route";
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!, {
+  fetch: (url, init) =>
+    fetch(url, {
+      ...init,
+      credentials: "include", // ⬅️ crucial for sending cookies (sessions)
+    }),
+});
